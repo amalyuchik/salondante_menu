aa2618006d5467ecfec902d72f17fdfe06f5faa6
andrei <amalyuchik@utah.org>
2019-08-15 07:51:50 -0600

    Initial commit

---
+++ src/main/java/com/salondante/sd_menu/SdMenuApplication.java
@@ -0,0 +1,13 @@
+package com.salondante.sd_menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdMenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salondante/sd_menu/SdMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salondante.sd_menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
